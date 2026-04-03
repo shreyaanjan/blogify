a199da63b63373e909b7174ef1d6b9079f5f109d
shreyaanjan <shreyaanjan02@gmail.com>
2026-04-03 20:00:09 +0530

    forgot password functionality done

--- routes/viewAuthRouter.js
+++ routes/viewAuthRouter.js
@@ -2,7 +2,8 @@ const express = require('express');
 const User = require('../models/UserModel.js');
 const router = express.Router()
 const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
+const sendMail = require('../utils/sendMail.js');
 
 router.get('/signup', (req, res) => {
     try {
@@ -17,6 +18,22 @@ router.get('/login', (req, res) => {
         return res.render('login')
     } catch (error) {
         console.log(error);
+    }
+})
+
+router.get('/forgot-password', (req, res) => {
+    try {
+        return res.render('forgotPassword')
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+router.get('/reset-password', (req, res) => {
+    try {
+        return res.render('resetPassword')
+    } catch (error) {
+        console.log(error)
     }
 })
 
@@ -69,11 +86,66 @@ router.post('/login', async (req, res) => {
             maxAge: 24 * 60 * 60 * 1000,
             httpOnly: true,
         })
-        
+
         return res.redirect('/')
     } catch (error) {
         console.log(error);
     }
 })
+
+router.post('/forgot-password', async (req, res) => {
+    try {
+        const { email } = req.body
+        const user = await User.findOne({ email })
+
+        if (!user) {
+            return res.redirect('/auth/forgot-password')
+        }
+
+        const OTP = parseInt(100000 + Math.random() * 900000).toString()
+        const hashedOTP = await bcrypt.hash(OTP, 10)
+
+        user.forgotPasswordOTP = hashedOTP
+        user.forgotPasswordOTPExpiry = Date.now() + 10 * 60 * 1000
+        await user.save()
+
+        await sendMail(email, "OTP To Reset Password For Blog App", `Your OTP To Reset Password is ${OTP}. This OTP is valid for 10 Minutes.`)
+
+        return res.redirect('/auth/reset-password')
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+router.post('/reset-password', async (req, res) => {
+    try {
+        const { email, otp, password } = req.body
+
+        const user = await User.findOne({ email })
+        if (!user) {
+            return res.redirect('/auth/reset-password')
+        }
+
+        if (!user.forgotPasswordOTP || !user.forgotPasswordOTPExpiry || user.forgotPasswordOTPExpiry < Date.now()) {
+            return res.redirect('/auth/forgot-password')
+        }
+
+        const isValidOTP = await bcrypt.compare(otp, user.forgotPasswordOTP)
+        if (!isValidOTP) {
+            return res.redirect('/auth/reset-password')
+        }
+
+        const newHashedPass = await bcrypt.hash(password, 10)
+        user.password = newHashedPass
+        user.forgotPasswordOTP = null
+        user.forgotPasswordOTPExpiry = null
+
+        await user.save()
+        return res.redirect('/auth/login')
+    } catch (error) {
+        console.log(error);
+    }
+})
+
 
 module.exports = router
